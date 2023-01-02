6d36d0316bc3350c4ad29b32f83cb8b6a720cb51
Daniele Campari <danielecampari@gmail.com>
2023-01-02 12:11:20 +0100

    Fix typo in comment (main.js)

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,20 +1,18 @@
-
-       /* TriggerEvent 
-* Attach listener and trigger the event
-* */
+/* TriggerEvent
+ * Attach listener and trigger the event
+ */
 export const triggerEvent = () => {
-  const trigger = document.querySelector('#drop-btn');
-  const target = document.querySelector('.dropdown-content');
-  trigger.addEventListener('click', () => {
-    toggleDisplay(target);
-  });
-
+    const trigger = document.querySelector('#drop-btn')
+    const target = document.querySelector('.dropdown-content')
+    trigger.addEventListener('click', () => {
+        toggleDisplay(target)
+    })
 }
 
-       /**
-        * @param {node} target - Node that fire event
-        */
+/**
+ * @param {node} target - Node that fire event
+ */
 export const toggleDisplay = (target) => {
-  let display = window.getComputedStyle(target).getPropertyValue('display');
-  target.style.setProperty('display', display === 'none' ? 'grid' : 'none');
-};
+    let display = window.getComputedStyle(target).getPropertyValue('display')
+    target.style.setProperty('display', display === 'none' ? 'grid' : 'none')
+}
