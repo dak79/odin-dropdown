60b5b9ca5663bd29295ccdab3de3e084e8660a8f
Daniele Campari <danielecampari@gmail.com>
2023-01-02 12:09:50 +0100

    Add comments to main.js

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,4 +1,7 @@
 
+       /* TriggerEvent 
+* Attach listener and trigger the event
+* */
 export const triggerEvent = () => {
   const trigger = document.querySelector('#drop-btn');
   const target = document.querySelector('.dropdown-content');
@@ -8,6 +11,9 @@ export const triggerEvent = () => {
 
 }
 
+       /**
+        * @param {node} target - Node that fire event
+        */
 export const toggleDisplay = (target) => {
   let display = window.getComputedStyle(target).getPropertyValue('display');
   target.style.setProperty('display', display === 'none' ? 'grid' : 'none');
